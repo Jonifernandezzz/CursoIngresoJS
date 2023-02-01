a431fcddb1b23bb8b7e898201ceaeee5c5854e84
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-01 15:56:28 -0300

    Jonifernandez

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,25 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+let sueldo;
+let nuevoSueldo;
+let aumento;
 
-	alert("ok");
+// para calcular el nuevo sueldo debo sumarle al sueldo original el aumento
+// para calcular un % tengo q multiplicar el valor por el porcentaje y lo divido por cien
+
+
+sueldo = parseFloat ( document.getElementById ("txtIdSueldo").value);
+
+
+aumento = sueldo * 10 / 100;
+
+nuevoSueldo = sueldo + aumento;
+
+
+document.getElementById ("txtIdResultado").value = nuevoSueldo;
+
+
+
+	
 }
