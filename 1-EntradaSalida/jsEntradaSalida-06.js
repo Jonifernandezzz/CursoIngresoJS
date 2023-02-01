7fa37b5d490aff3bb51c4b962fe5b81c13714c71
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-01 10:55:37 -0300

    jonifernandez

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,19 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numunoT
+	let numdosT
+    let numuno
+	let numdos
+	numunoT= document.getElementById ("txtIdNumeroUno").value;
+    numdosT = document.getElementById ("txtIdNumeroDos").value;
+	
+   numuno= parseInt (numunoT);
+   numdos = parseInt (numdosT);
+
+	
+	
+	alert(numuno + numdos);
+
 }
 
