f76d113fdcca96c590a7f70f78b8a22edf384910
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-01 14:10:22 -0300

    Jonifernandez

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,22 +4,103 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	alert("ok");	
+{
+	let numeroUnoT;
+	let numeroDosT;
+	let numeroUno;
+	let numeroDos;
+	let mensaje;
+	
+	numeroUnoT = document.getElementById ("txtIdNumeroUno").value;
+	numeroDosT = document.getElementById ("txtIdNumeroDos").value;
+
+   numeroUno = parseInt (numeroUnoT);
+   numeroDos = parseInt (numeroDosT);
+
+   mensaje = " la suma es " + (numeroUno + numeroDos) ;
+
+
+	alert(mensaje);	
 }
 
 function restar()
-{
+{  
+	let numeroUnoT;
+	let numeroDosT;
+	let numeroUno;
+	let numeroDos; 
+	let mensaje;
+
+	numeroUnoT = document.getElementById ("txtIdNumeroUno").value;
+	numeroDosT = document.getElementById ("txtIdNumeroDos").value;
+
+   numeroUno = parseInt (numeroUnoT);
+   numeroDos = parseInt (numeroDosT);
+
+   mensaje = " la resta es " + (numeroUno - numeroDos) ;
+
+
+	alert(mensaje);	
 	
-}
+
+	
+
+}   
+
 
 function multiplicar()
 { 
+	let numeroUnoT;
+	let numeroDosT;
+	let numeroUno;
+	let numeroDos;
+	let mensaje
+ 
+	numeroUnoT = document.getElementById ("txtIdNumeroUno").value;
+	numeroDosT = document.getElementById ("txtIdNumeroDos").value;
+
+   numeroUno = parseInt (numeroUnoT);
+   numeroDos = parseInt (numeroDosT);
+
+   mensaje = " la multiplicacion es " + numeroUno * numeroDos ;
+
+
+	alert(mensaje);	
 	
-}
+
+
+
+	
+}   
 
 function dividir()
 {
+	let numeroUnoT;
+	let numeroDosT;
+	let numeroUno;
+	let numeroDos;
+	let mensaje;
+
+	numeroUnoT = document.getElementById ("txtIdNumeroUno").value;
+	numeroDosT = document.getElementById ("txtIdNumeroDos").value;
+
+   numeroUno = parseInt (numeroUnoT);
+   numeroDos = parseInt (numeroDosT);
+
+   mensaje = " la multiplicacion es " + numeroUno / numeroDos ;
+
+
 	
-}
+
+
+	
+	
+	alert(mensaje);
+
+
+
+
+
+}  
+
 
