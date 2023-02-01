c00ae41c1d6efce7e82c5c30222229a68ed46341
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-01 16:08:33 -0300

    Jonifernandez

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,27 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+
+	let importe;
+	let importefinal;
+	let descuento;
+	
+	// para calcular el nuevo importe debo sumarle al importe original el Descuento
+	// para calcular un % tengo q multiplicar el valor por el porcentaje y lo divido por cien
+	
+	
+	importe = parseFloat ( document.getElementById ("txtIdSueldo").value);
+	
+	
+	Descuento = importe * 25 / 100;
+	
+	importefinal = importe - Descuento;
+	
+	
+	
+	document.getElementById ("txtIdResultado").value = importefinal;
+	
+
+
+
 }
