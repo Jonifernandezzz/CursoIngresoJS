c518bc064c6b4076b251670912241b378f75968a
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-02 12:31:20 -0300

    Jonifernandez

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -4,15 +4,68 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
+
+
+
 function Rectangulo () 
 {
+//  A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+// para sacar primetro se hace * 2 + base * 2
+//para hacer 3 hilos de alambre se tiene q hacer perimetro * 3/
+
+let largo;
+let ancho;
+let perimetro;
+let alambre;
+
+largo = document.getElementById("txtIdLargo").value;
+ancho = document.getElementById("txtIdAncho").value;
+
+
+largo = parseFloat(largo);
+ancho = parseFloat(ancho);
+
+perimetro = largo * 2 + ancho * 2;
+
+alambre = perimetro * 3;
+
+
+
+
+
 
 }
+
+
+
 function Circulo () 
 {
-	
+/* B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+para sacar el perimetro de un circulo hay q hacer 2 * pi (3,14) * R*/
+
+let radio;
+let perimetro;
+let alambre;
+
+    
+
+
+
+
+
 }
 function Materiales () 
 {
-	
+
+ /* C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+ para sacar el area 
+ para sacar el area de un rectangulo se hace largo * ancho
+ para sacar el cemento y el cal multiplico metro cuadrado por cemento y luego cal*/
+ 
+
+
+
+
+
+
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,10 +1,23 @@
 /*3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+(32 °F − 32) × 5/9
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+(0 °C × 9/5) + 32
 */
 function FahrenheitCentigrados () 
 {
-	
+
+ 
+let fahr;
+let centidrados;
+
+fahr = document.getElementById ("txtIdTemperatura").value;
+fahr = parseFloat(fahr);
+centidrados = (fahr  - 32) * 5/9;
+
+alert(fahr + " Fahrenheit son : " + centidrados);
+
+
 }
 
 function CentigradosFahrenheit () 
