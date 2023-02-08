8d6619543c70fd1e7e83d4eca3549ef53a425a10
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-08 09:24:09 -0300

    Joni fernandez

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,21 @@
+//Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
+
+// txtIdEdad estadoCivil  > <
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+  let edad;
+  let estadoCivil;
+  let mensaje;
+  edad = document.getElementById("txtIdEdad").value;
+  edad = parseInt(edad);
 
+  estadoCivil = document. getElementById("estadoCivil").value;
 
-}//FIN DE LA FUNCIÓN
+  if (edad < 18 && estadoCivil != "Soltero")
+{
+   
+    mensaje = " Es muy pequeño para NO ser soltero. "
+    alert (mensaje);
+
+}
+}
