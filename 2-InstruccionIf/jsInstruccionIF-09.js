2664b4a1577a731f05ddd316cfa5c87f21384c1d
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-08 11:21:55 -0300

    Joni fernandez

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,17 @@
+// Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive
+
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+// se definen las variables necesarias
+let numRandom;
+let max = 10;
+let min = 1;
+//se genera el numero random entre 1 y 10
+numRandom = Math.floor(Math.random() * max + min);
+	
+console.log(numRandom);
+
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,36 @@
+/* Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4 */
+//  > <
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+ let num;
+ let mensaje;
+ 
+ num = Math.floor(Math.random() * 10 + 1);
+
+ if (num  >= 9)
+{
+mensaje = num + " EXCELENTE "
+
+}
+else 
+{
+if (num  >= 4 )
+{
+    mensaje = num +  " APROBADO "	
+}
+else {
+	  mensaje = num + " REPROBADO "
+}
+}
+  alert(mensaje);
+
+	
+
+
+
 
 }//FIN DE LA FUNCIÓN
