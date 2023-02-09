2ee57811bcaa919514913ac054bcf7dd2ea892c5
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-09 08:55:00 -0300

    Joni fernandez

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -5,31 +5,115 @@ B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuent
 C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-
- */
-// descuento = 50
+”Usted pago X de IIBB.”, siendo X el impuesto que se pagó.  */
 // > <
+
+
 function CalcularPrecio () 
 {
-let lamparas;
-let preciolam;
+let precio;    
+let cantidad;
+let descuento;
 let marca;
+let precioFinal;
+let precioConDescuento;
+let descuentoAplicado;
+let importeFinal;
 
- 
- preciolam = 35
- lamparas = document.getElementById("txtIdCantidad").value;
- lamparas = parseInt(lamparas);
- marca = document.getElementById("Marca").value;
+precio = 35;
+descuento = 50;
 
- 
-if ( lamparas >= 6 )
+
+cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+marca = document.getElementById("Marca").value;
+
+if(cantidad>= 6)
 {
- preciolam = preciolam - preciolam * (50/100);
+  precioFinal = precio * cantidad;
+  descuentoAplicado = precioFinal * descuento/100;
+  precioConDescuento = precioFinal - descuentoAplicado;
+  
+  //precio final = total - descuento;
+   
+  // mensaje = total;
+
+  document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+}
+else if (cantidad==5 && marca == "ArgentinaLuz")
+{
+    descuento = 40;
+    precioFinal = precio * cantidad;
+    descuentoAplicado = precioFinal * descuento/100;
+    precioConDescuento = precioFinal - descuentoAplicado;
+    
+
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+}
+else if (cantidad==5 && marca != "ArgentinaLuz")
+{
+    descuento = 30;
+    precioFinal = precio * cantidad;
+    descuentoAplicado = precioFinal * descuento/100;
+    precioConDescuento = precioFinal - descuentoAplicado;
+    
+
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+}
+else if (cantidad ==4 && marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+{
+    descuento = 25;
+    precioFinal = precio * cantidad;
+    descuentoAplicado = precioFinal * descuento/100;
+    precioConDescuento = precioFinal - descuentoAplicado;
+    
+
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+}
+else if (cantidad==4 && marca != "ArgentinaLuz" || marca != "Felipelamparas")
+
+{
+    descuento = 20;
+    precioFinal = precio * cantidad;
+    descuentoAplicado = precioFinal * descuento/100;
+    precioConDescuento = precioFinal - descuentoAplicado;
+
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+}
+ 
+ if (cantidad==3 && marca == "ArgentinaLuz")
+{
+descuento = 15;
+precioFinal = precio * cantidad;
+descuentoAplicado = precioFinal * descuento/100;
+precioConDescuento = precioFinal - descuentoAplicado;
+
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
 
 }
 
+else if (cantidad==3 && marca == "FelipeLamparas")
+{
+descuento = 10;
+precioFinal = precio * cantidad;
+descuentoAplicado = precioFinal * descuento/100;
+precioConDescuento = precioFinal - descuentoAplicado;
 
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+}
+
+else if (cantidad==3 && marca != "Argentinaluz" || marca != "FelipeLamparas")
+{
+descuento = 5;
+precioFinal = precio * cantidad;
+descuentoAplicado = precioFinal * descuento/100;
+precioConDescuento = precioFinal - descuentoAplicado;
+
+document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+}
 
 
 
