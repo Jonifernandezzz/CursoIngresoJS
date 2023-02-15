e733c4dc7af2a7893b016156007f2d78ac346bb9
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-15 08:49:40 -0300

    Joni fernandez

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -3,9 +3,30 @@ Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
-	var sexoIngresado;
+	let sexoIngresado;
+	let respuesta;
 	sexoIngresado = prompt("ingrese f ó m .");
+	
 
+	
+
+	
+
+	while(respuesta == f || respuesta == m )
+
+	{
+     if(respuesta==f)
+	 { 
+		mensaje = "Usted es de sexo femenino";
+
+	 }
+	 else(respuesta==m)
+	 {
+	    mensaje = "Usted es de sexo masculino";
+	 }
+	 
+	}
+	document.getElementById("txtIdNumero").value = sexoIngresado;
 
 
 	txtIdSexo.value=sexoIngresado;
