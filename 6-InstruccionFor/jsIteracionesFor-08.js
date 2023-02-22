264dea5ccefd5ca371e3fb8bc17391cf3a185141
Jonifernandezzz <imgsdestroyerbo3@gmail.com>
2023-02-22 18:48:52 -0300

    Joni fernandez

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -12,17 +12,26 @@ function mostrar()
 	numero = prompt("Ingrese un numero: ")
 	while(isNaN(numero))
 	{
-		numero = prompt("Error, ingrese un numero")
+		numero = prompt("Error, ingrese un numero");
 	}
 
 	for(let i=2; i  < numero; i++)
 	{
       if(numero%i == 0)
 	  {
+		contadordivisores = contadordivisores + 1;
         console.log(numero)
 	  }
 
     }
+
+	if (contadordivisores == 2)
+	{
+		console.log(numero + " es primo ");
+	}else
+	{
+		console.log(numero + " no es primo ");
+	}
 	
 
 }//FIN DE LA FUNCIÓN
